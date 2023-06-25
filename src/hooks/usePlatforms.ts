82d5a24fcec7a1099ab4ee6255723d5dce19c5c3
KASHWAY <targosz.hubert@gmail.com>
2023-06-25 17:02:58 +0200

    Refactoring the code

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import apiClient from "../services/api-client";
 import { FetchResponse } from "./useData";
+import platforms from '../data/platforms';
 
 interface Platform {
   id: number;
