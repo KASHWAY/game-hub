b860c942c7ba759a3223da72e4e0f9c27f320833
KASHWAY <targosz.hubert@gmail.com>
2023-06-28 09:45:13 +0200

    Simplifying time calculations with MS library.

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
 import APIClient from "../services/api-client";
 import platforms from '../data/platforms';
+import ms from 'ms';
+
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 
@@ -13,7 +15,7 @@ export interface Platform {
 const usePlatforms = () => useQuery({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24hours
+    staleTime: ms('24h'),
     initialData: platforms,
 });
 
